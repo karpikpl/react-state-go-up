382706014d833b0b98c5a13e5d79fb182973ad6a
Piotr Karpala <karpik.pl@gmail.com>
2018-06-09 22:08:41 +0200

    init commit
    Signed-off-by: Piotr Karpala <karpik.pl@gmail.com>

---
+++ src/Calculator.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import BoilingVerdict from './BoilingVerdict';
+import TemperatureInput from './TemperatureInput';
+
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temperature: '',
+      scale: 'c'
+    };
+  };
+
+  onTemperatureChangeCelsius = (value) => {
+    this.setState({temperature: value, scale: 'c'});
+  }
+
+  onTemperatureChangeFahrenHeit = (value) => {
+    this.setState({temperature: value, scale: 'f'});
+  }
+
+  render = () => {
+    const temperature = this.state.temperature;
+    const scale = this.state.scale;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (<div>
+      <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.onTemperatureChangeFahrenHeit}/>
+      <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.onTemperatureChangeCelsius}/>
+      <BoilingVerdict celsius={celsius}/>
+    </div>);
+  }
+}
+
+export default Calculator;
